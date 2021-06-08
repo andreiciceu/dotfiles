bfdb6192c2b740922ae1f9ceb92a8b39565babb4
Andrei <drey@192-168-0-166.rdsnet.ro>
2021-06-09 02:42:12 +0300

    setup update

---
+++ setup-keys.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+cp secrets/so-prod.pk ~/.ssh/
+chmod 600 ~/.ssh/so-prod.pk
+
+cp secrets/id_rsa ~/.ssh/
+chmod 600 ~/.ssh/id_rsa
+
+cp secrets/id_rsa.pub ~/.ssh/
+chmod 600 ~/.ssh/id_rsa.pub
+
+cp secrets/kubeconfig ~/.kubeconfig
+
+cp secrets/atac-ssh.sh ~/
+chmod +x ~/atac-ssh.sh
+
+cp secrets/sounderas-ssh.sh ~/
+chmod +x ~/sounderas-ssh.sh
+
--- setup.sh
+++ setup.sh
@@ -3,8 +3,13 @@
 # brew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
-# dotfiles manager
+# tools
 brew install yadm
+brew install --cask qbittorrent
+brew install --cask spotify
+brew install --cask google-chrome
+brew install --cask firefox
+brew install --cask bitwarden
 
 # ux
 brew install --cask amethyst
@@ -12,12 +17,26 @@ brew install --cask amethyst
 # dev tools
 brew install --cask iterm2
 brew install --cask visual-studio-code
+brew install --cask postman
+brew install --cask fork
+
+## CLI additions
+brew install watch
+
+## container
 brew install docker
+brew install kubectl
+brew install helm
+
+## xcode
+xcode-select --install
 
 ## js
 brew install yarn
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
+nvm install node
 
 ## android
 brew install --cask android-studio
 brew install openjdk
+
