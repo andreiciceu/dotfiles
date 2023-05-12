b4b12abb69d389741b44a612913d5f5a0e1609d7
Andrei Ciceu <andreiciceu@users.noreply.github.com>
2023-05-12 11:59:21 +0300

    Update setup.sh

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
 
-# brew
+# initial setup
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+# yadm
+brew install yadm
+yadm clone https://github.com/andreiciceu/dotfiles
+yadm status
 
 # tools
-brew install yadm
 brew install --cask qbittorrent
 brew install --cask spotify
 brew install --cask google-chrome
@@ -12,27 +15,15 @@ brew install --cask firefox
 brew install --cask slack
 brew install --cask iina
 
-# ux
-# brew install --cask amethyst
-
 # dev tools
 brew install --cask iterm2
 brew install --cask visual-studio-code
 brew install --cask postman
 brew install --cask fork
 brew install --cask lens
+brew install --cask tableplus
 brew install --cask db-browser-for-sqlite
-
-# python
-# curl https://bootstrap.pypa.io/pip/2.7/get-pip.py -o get-pip.py
-# python get-pip.py
-
-
-## CLI additions
-brew install watch
-### OpenStack
-pip install python-openstackclient
-pip install python-magnumclient
+brew install --cask figma
 
 ## container
 brew install --cask docker
@@ -53,6 +44,13 @@ xcode-select --install
 sudo gem install cocoapods
 sudo gem install fastlane
 
-# env
-yadm clone https://github.com/andreiciceu/dotfiles
-yadm status
+## CLI additions
+brew install watch
+
+### OpenStack
+# pip install python-openstackclient
+# pip install python-magnumclient
+
+# python
+# curl https://bootstrap.pypa.io/pip/2.7/get-pip.py -o get-pip.py
+# python get-pip.py
